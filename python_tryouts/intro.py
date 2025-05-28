10cc45f5fea29c4bc5046170bd9f7b38dbc67b4c
ivan kaweesa <lilswings7@gmail.com>
2025-05-28 20:44:17 +0000

    Added intro.py with hello world and variables

---
+++ python_tryouts/intro.py
@@ -0,0 +1,13 @@
+# Enter your name and age
+name = input("Your Name. ")
+age = input("your age. ")
+
+# Enter your favourite food and color.
+food = input("Your favourite food. ")
+colour = input("Your favourite colour. ")
+
+
+
+print(f"Hello, my name is {name} and I am {age} years old.")
+print(f"My favourite food is {food}")
+print(f"My favourite colour is {colour}")
