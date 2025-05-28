45944916c1d6ee4d500cb8e39090833251cf47ca
ivan kaweesa <lilswings7@gmail.com>
2025-05-28 21:42:31 +0000

    Basic mathematics

---
+++ python_tryouts/calc.py
@@ -0,0 +1,14 @@
+# Simple calculator
+a = int(input("enter 1st number. "))
+b = int(input("enter 2nd number. "))
+
+Add = a + b
+Subtract = a - b
+Divide = a / b
+Multiply = a * b
+
+
+print(f"a + b = {Add:.1f}")
+print(f"a - b = {Subtract:.1f}")
+print(f"a / b = {Divide:.1f}")
+print(f"a * b = {Multiply:.1f}")
