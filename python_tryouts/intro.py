553f0b82291d49421c192c3bf2be0f370a275e85
ivan <ivankaweesa.biz@gmail.com>
2025-05-30 02:03:49 +0200

    Added some new changes in intro.py

--- python_tryouts/intro.py
+++ python_tryouts/intro.py
@@ -1,14 +1,15 @@
 # Enter your name and age
-name = input("Your Name. ")
-age = input("your age. ")
+name = input("/n Enter your Name: ")
+age = int(input("/n Enter your age: "))
 
 # Enter your favourite food and color.
-food = input("Your favourite food. ")
-colour = input("Your favourite colour. ")
-Height = int(input("How tall are u. "))
+food = input("/n What is your favourite food?: ")
+colour = input("/n What is your favourite colour?: ")
+Height = int(input("/n How tall are u?: "))
 
 
 
 print(f"Hello, my name is {name} and I am {age} years old.")
 print(f"My favourite food is {food}")
 print(f"My favourite colour is {colour}")
+
