bb682d58df1e4f8fe0346db9225a56614aa3e9ba
ivan <ivankaweesa.biz@gmail.com>
2025-05-31 14:57:18 +0200

    A guessing game

--- match_case_statements/guess_game.py
+++ match_case_statements/guess_game.py
@@ -1,9 +1,12 @@
 import random # So we can pick a secret number.
 
 play = "yes"
+
 while play == "yes":
     secret_number = random.randint(1, 11)
-    guess = int(input("enter any number (1-11): "))
+
+print("Guess a number between 1 and 11")
+guess = int(input("enter any number: "))
 
 match guess:
   case _ if guess == secret_number:
