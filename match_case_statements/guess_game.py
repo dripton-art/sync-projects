3f689e095444e82f9f09b2b7d39c890b73544f34
ivan <ivankaweesa.biz@gmail.com>
2025-05-31 14:41:40 +0200

    Created a guess game.

---
+++ match_case_statements/guess_game.py
@@ -0,0 +1,22 @@
+import random # So we can pick a secret number.
+
+play = "yes"
+while play == "yes":
+    secret_number = random.randint(1, 11)
+    guess = int(input("enter any number (1-11): "))
+
+match guess:
+  case _ if guess == secret_number:
+    print("Congratulations, you guessed!")
+  case _ if guess > secret_number:
+    print("Oops, your guess is a bit high. Try again!")
+  case _ if guess < secret_number:
+    print("Nope, your guess is a bit low. Give it another shot!")
+
+# Ask if the user wants to play again.
+play = input("Do you want to play again? (yes or no): ").lower()
+
+if play == "yes":
+    print("Lets play again!")
+else:
+    print("Thank you for playing!")
