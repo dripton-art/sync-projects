fd776efb1128654fcee7fe2d78938fb1cc3d2775
ivan <ivankaweesa.biz@gmail.com>
2025-06-06 01:19:30 +0200

    simple calculator

--- python_tryouts/calc.py
+++ python_tryouts/calc.py
@@ -4,11 +4,14 @@ b = int(input("enter 2nd number. "))
 
 Add = a + b
 Subtract = a - b
-Divide = a / b
 Multiply = a * b
-
 
 print(f"a + b = {Add:.1f}")
 print(f"a - b = {Subtract:.1f}")
-print(f"a / b = {Divide:.1f}")
 print(f"a * b = {Multiply:.1f}")
+
+if b == 0:
+    print("\n*cannot be divided by zero*")
+else:
+    Divide = a / b
+    print(f"a / b = {Divide:.1f}")
